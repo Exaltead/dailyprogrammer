54b5795ebb35857fd29f05aa6d58b0678c795b9c
Aku <aku.pohjola@student.tut.fi>
2017-06-28 01:06:16 +0300

    Improved readability and added IO

--- SIR_model_intermdiate/sir_model.erl
+++ SIR_model_intermdiate/sir_model.erl
@@ -1,10 +1,14 @@
 -module(sir_model).
--export ([sir/5]).
+-export ([simulate/0]).
 
+simulate() ->
+    {ok, [Systems, Infected, SI, IR, SR]} = read_io_command(),
+    sir(Systems, Infected, SI, IR, SR).
 
 sir(Systems, Infected, SI, IR, SR) ->
     print_stats(Systems - Infected, Infected, 0, 0),
     sir_simulation(Systems - Infected, Infected, 0, SI, IR, SR, 1).
+
 
 sir_simulation(0, 0, _, _, _, _, Iteration) ->
     print_finish(Iteration);
@@ -12,37 +16,40 @@ sir_simulation(S, I, R, SI, IR, SR, Iteration) ->
     Infected = simulate_change(S, SI, 0),
     Cured = simulate_change(I, IR, 0),
     Patched = simulate_change(S - Infected, SR, 0),
-    print_stats(S - Infected - Patched, I - Cured + Infected,
-    R + Patched + Cured, Iteration),
-    sir_simulation(S - Infected - Patched, I - Cured + Infected,
-    R + Patched + Cured, SI, IR, SR, Iteration + 1).
-
+    % For clarity the new caleus are calculated in advance
+    New_s = S - Infected - Patched,
+    New_i = I - Cured + Infected,
+    New_r = R + Patched + Cured,
+    print_stats(New_s, New_i, New_r, Iteration),
+    sir_simulation(New_s, New_i, New_r, SI, IR, SR, Iteration + 1).
 
 
 simulate_change(0, _, Remaining) ->
     Remaining;
-simulate_change(Systems, Ratio, Remaining) ->
-    Changed = state_changed(Ratio),
-    case Changed of
-        true ->
-            simulate_change(Systems - 1, Ratio, Remaining + 1);
+simulate_change(Systems, Proba, Remaining) ->
+    case system_state_changed(Proba) of
         false ->
-            simulate_change(Systems - 1, Ratio, Remaining)
+            simulate_change(Systems - 1, Proba, Remaining + 1);
+        true ->
+            simulate_change(Systems - 1, Proba, Remaining)
     end.
 
-state_changed(Ratio)->
-    Ratio > rand:uniform().
+system_state_changed(Proba)->
+    Proba > rand:uniform().
 
-
+% IO part of the system
 print_finish(Iteration) ->
     io:format("On gention ~w simualton ened with all systems resistant", [Iteration]).
-
 
 print_stats(S, I, R, Gen) ->
     SIR_size = length(integer_to_list(S + I + R)),
     % Format the text so that only the numbers increment
+    % and placement remains the same.
     FS = string:pad(integer_to_list(S), SIR_size),
     FI = string:pad(integer_to_list(I), SIR_size),
     FR = string:pad(integer_to_list(R), SIR_size),
-    Message = io_lib:format("S -> ~s I -> ~s R -> ~s on iteration ~w ~n", [FS, FI, FR, Gen]),
-    io:format(Message).
+    io:format("S -> ~s I -> ~s R -> ~s on iteration ~w ~n", [FS, FI, FR, Gen]).
+
+read_io_command() ->
+    Prompt = "Input the simulation parameters: Systems Infected SI-rate IR-rate RI-rate:\n ",
+    io:fread(Prompt, "~d ~d ~f ~f ~f").
